d3ac76aa81690c599835c68099092c370a4f8d81
Obiedere Okezie Frank <obiedereokezie@gmail.com>
2022-08-03 01:20:30 +0100

    prettier fix *1

--- src/services/book.ts
+++ src/services/book.ts
@@ -1,41 +1,41 @@
-import axios from "axios";
+import axios from 'axios';
 
 import { api } from '../utils';
-import { CommentModel , Comment } from '../models';
+import { CommentModel } from '../models';
 
 export interface Model {
-    comment: CommentModel;
+  comment: CommentModel;
 }
 
 interface Book {
-    readonly url: string;
-    readonly name: string;
-    readonly isbn: string;
-    readonly authors: string[];
-    readonly numberOfPages: number;
-    readonly publisher: string;
-    readonly country: string;
-    readonly mediaType: string;
-    readonly reLeased: string;
-    readonly characters: string[];
-    readonly povCharacters: string[];
-    comments?: number;
+  readonly url: string;
+  readonly name: string;
+  readonly isbn: string;
+  readonly authors: string[];
+  readonly numberOfPages: number;
+  readonly publisher: string;
+  readonly country: string;
+  readonly mediaType: string;
+  readonly reLeased: string;
+  readonly characters: string[];
+  readonly povCharacters: string[];
+  comments?: number;
 }
 
 export class BookServices {
-    constructor(readonly model: Model) {
-        this.list = this.list.bind(this)
-    }
+  constructor(readonly model: Model) {
+    this.list = this.list.bind(this);
+  }
 
-    async list(): Promise<Book[]> {
-        const books = await (await axios.get<Book[]>(`${api}books`)).data;
-        if (books.length > 0) {
-            books.sort((bookA: Book, bookB: Book) => bookA.reLeased.localeCompare(bookB.reLeased))
-            books.forEach(async (book) => {
-                const bookId = `${book.url[book.url.length - 1]}`;
-                book.comments = await (await this.model.comment.listByBook(bookId))?.length;
-            });
-        }
-        return books;
+  async list(): Promise<Book[]> {
+    const books = await (await axios.get<Book[]>(`${api}books`)).data;
+    if (books.length > 0) {
+      books.sort((bookA: Book, bookB: Book) => bookA.reLeased.localeCompare(bookB.reLeased));
+      books.forEach(async (book) => {
+        const bookId = `${book.url[book.url.length - 1]}`;
+        book.comments = await (await this.model.comment.listByBook(bookId))?.length;
+      });
     }
+    return books;
+  }
 }
